ea582076a8eea78ec2cf328b590bdbdf499a0e34
chiozzi <chiozziletici0114@gmail.com>
2025-03-21 11:13:02 -0300

    atualização

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,31 @@
-import { NumberSymbol } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
   standalone: false,
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Comparador';
 
-
   num1: number | null = null;
   num2: number | null = null;
   resultado: string = '';
+  comparacao: string = '';
 
-
-    verificarnumeros(){
-      if (this.num1 !== null && this.num2 !== null) {
-        if (this.num1 > this.num2){
-          this.resultado = 'MAAIOR';
-        } else if (this.num2 > this.num1){
-          this.resultado = 'MENOR';
-        } else
-          this.resultado = 'IGUAL';
-        
+  verificarnumeros() {
+    if (this.num1 !== null && this.num2 !== null) {
+      if (this.num1 > this.num2) {
+        this.resultado = 'O primeiro número é MAIOR que o segundo.';
+        this.comparacao = `${this.num1} > ${this.num2}`;
+      } else if (this.num2 > this.num1) {
+        this.resultado = 'O primeiro número é MENOR que o segundo.';
+        this.comparacao = `${this.num2} > ${this.num1}`;
+      } else {
+        this.resultado = 'O primeiro número é IGUAL ao segundo.';
+        this.comparacao = `${this.num1} = ${this.num2}`;
+      }
     }
   }
 }
