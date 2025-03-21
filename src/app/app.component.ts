cd7230e554d3a67e63b88b3670187241e2e5df33
chiozzi <chiozziletici0114@gmail.com>
2025-03-20 23:27:24 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { NumberSymbol } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Comparador';
+
+
+  num1: number | null = null;
+  num2: number | null = null;
+  resultado: string = '';
+
+
+    verificarnumeros(){
+      if (this.num1 !== null && this.num2 !== null) {
+        if (this.num1 > this.num2){
+          this.resultado = 'MAAIOR';
+        } else if (this.num2 > this.num1){
+          this.resultado = 'MENOR';
+        } else
+          this.resultado = 'IGUAL';
+        
+    }
+  }
 }
